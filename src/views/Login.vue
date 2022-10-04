644ae6aa28d427535077fe6c491aed5d8a59c9d5
peterjtz <jiaotianze123@gmail.com>
2022-10-03 22:05:27 -0700

    make it better

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,7 +14,7 @@
             />
             <p v-if="errorText" class="text-danger">{{ errorText }}</p>
           </div>
-          <button type="submit" class="btn btn-primary">Gusset Login</button>
+          <button type="submit" class="btn btn-primary">Guest Login</button>
         </form>
         <p></p>
         <div>
@@ -25,7 +25,7 @@
         <p></p>
         <div>
           <container>
-            <button @click="googleLogin()">Goggle Sign In</button>
+            <button @click="googleLogin()">Google Sign In</button>
           </container>
         </div>
       </div>
@@ -43,10 +43,12 @@ export default {
       name: "",
       errorText: null,
       userList: [],
+      emailList: [],
     };
   },
   created() {
     let ref = fb.collection("username");
+    let ref2 = fb.collection("email");
 
     ref.onSnapshot((snapshot) => {
       snapshot.docChanges().forEach((change) => {
@@ -57,24 +59,50 @@ export default {
         }
       });
     });
+
+    ref2.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.emailList.push(doc.data().mail);
+          //debugger;
+        }
+      });
+    });
   },
   methods: {
     login() {
-      //debugger;
-      if (this.userList.includes(this.name)) {
-        this.errorText = "Username alread exists!";
-      } else if (this.name) {
-        fb.collection("username")
-          .add({
-            name: this.name,
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-        this.$router.push({ name: "Chat", params: { name: this.name } });
-      } else {
+      if (this.name === "") {
         this.errorText = "Please enter a name first!";
+        return;
       }
+      //debugger;
+      function assignRandom(name) {
+        let randomNumber1 = Math.floor(Math.random() * 10);
+        let randomNumber2 = Math.floor(Math.random() * 10);
+        let randomNumber3 = Math.floor(Math.random() * 10);
+        let randomNumber4 = Math.floor(Math.random() * 10);
+        name =
+          name +
+          " #" +
+          randomNumber1 +
+          randomNumber2 +
+          randomNumber3 +
+          randomNumber4;
+        return name;
+      }
+      this.name = assignRandom(this.name);
+      while (this.userList.includes(this.name)) {
+        this.name = assignRandom(this.name);
+      }
+      fb.collection("username")
+        .add({
+          name: this.name,
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.$router.push({ name: "Chat", params: { name: this.name } });
     },
 
     accountLogin() {
@@ -82,6 +110,20 @@ export default {
     },
 
     googleLogin() {
+      function assignRandom(name) {
+        let randomNumber1 = Math.floor(Math.random() * 10);
+        let randomNumber2 = Math.floor(Math.random() * 10);
+        let randomNumber3 = Math.floor(Math.random() * 10);
+        let randomNumber4 = Math.floor(Math.random() * 10);
+        name =
+          name +
+          " #" +
+          randomNumber1 +
+          randomNumber2 +
+          randomNumber3 +
+          randomNumber4;
+        return name;
+      }
       const provider = new firebase.auth.GoogleAuthProvider();
       firebase
         .auth()
@@ -91,18 +133,50 @@ export default {
           if (user.email === "jiaotianze123@gmail.com") {
             this.$router.push({ name: "Admin" });
           } else {
-            if (!this.userList.includes(this.name)) {
+            if (!this.emailList.includes(user.email)) {
+              let temp = assignRandom(user.displayName);
+              while (this.userList.includes(temp)) {
+                temp = assignRandom(temp);
+              }
               fb.collection("username")
                 .add({
-                  name: this.name,
+                  name: temp,
                 })
                 .catch((err) => {
                   console.log(err);
                 });
+
+              fb.collection("email")
+                .add({
+                  name: temp,
+                  mail: user.email,
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+              this.$router.push({
+                name: "Chat",
+                params: { name: temp },
+              });
             }
-            this.$router.push({
-              name: "Chat",
-              params: { name: user.displayName },
+
+            let ref = fb.collection("email");
+            let userName = "";
+
+            ref.onSnapshot((snapshot) => {
+              snapshot.docChanges().forEach((change) => {
+                if (change.type == "added") {
+                  let doc = change.doc;
+                  let mail = doc.data().mail;
+                  if (mail === user.email) {
+                    userName = doc.data().name;
+                    this.$router.push({
+                      name: "Chat",
+                      params: { name: userName },
+                    });
+                  }
+                }
+              });
             });
           }
         })
@@ -122,5 +196,10 @@ export default {
   display: block;
   margin-left: auto;
   margin-right: auto;
+}
+
+#app {
+  text-align: center;
+  size: 500vmax;
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,115 +1,133 @@
 <template>
-    <div class="container">
-        <div class="card sign up">
-            <div class="card-body">
-                <h2 class="card-title text-center">Account Sign Up</h2>
-                <form @submit.prevent="signup" class="text-center">
-                    <div class="form-group">
-                        <input type="text" placeholder="Username" name = "name" v-model="name" />
-                        <br />
-                        <input type="email" placeholder="Emial" v-model="email" />
-                        <br />
-                        <input type="password" placeholder="Password" v-model="password" />
-                        <br />
-                        <p v-if="errorText" class="text-danger">{{errorText}}</p>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign Up</button>
-                </form>
-                <p>
-                    Have an account?
-                    <router-link to="/">Login</router-link>
-                </p>
-            </div>
-        </div>
+  <div class="container">
+    <div class="card sign up">
+      <div class="card-body">
+        <h2 class="card-title text-center">Account Sign Up</h2>
+        <form @submit.prevent="signup" class="text-center">
+          <div class="form-group">
+            <input
+              type="text"
+              placeholder="Username"
+              name="name"
+              v-model="name"
+            />
+            <br />
+            <input type="email" placeholder="Emial" v-model="email" />
+            <br />
+            <input type="password" placeholder="Password" v-model="password" />
+            <br />
+            <p v-if="errorText" class="text-danger">{{ errorText }}</p>
+          </div>
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>
+        <p>
+          Have an account?
+          <router-link to="/">Login</router-link>
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import firebase from "firebase";
-    import fb from "@/firebase/init";
-    export default{
-        name:"Signup",
-        data(){
-            return{
-                name: "",
-                errorText: null,
-                email: "",
-                password: "",
-                userList: []
-            }
-        },
-        created(){
-      let ref = fb.collection("username");
-  
-        ref.onSnapshot((snapshot) => {
-          snapshot.docChanges().forEach((change) => {
-            if (change.type == "added") {
-              let doc = change.doc;
-              this.userList.push(doc.data().name);
-              //debugger;
-            }
-          });
-        });
-        },
-        methods:{
-            signup: function(){
-                if(!this.name)
-                {
-                    this.errorText = "Please enter a name first!";
-                    return;
-                }
-
-                else if(this.userList.includes(this.name)){
-                    this.errorText = "Username alread exists!";
-                    return;
-                }
-                this.errorText=null;
-                // Log the user in
-                firebase
-                .auth()
-                .createUserWithEmailAndPassword(this.email, this.password)
-                .then((result) => {
-                    const user = result.user;
-                    user.updateProfile({displayName: this.name});
-                    alert("Account has been created successfully!");
-
-                    // Add username to database
-                    fb.collection("username")
-                    .add({
-                    name: this.name,
-                    })
-                    .catch((err) => {
-                    console.log(err);
-                    });
-
-                    // Redirect to sign in Page
-                    this.$router.push({name:'Signin'});
-                }).catch((error) => {
-                alert(error.message);
-                console.log(error.code);
-                });
-            }
-        }
+import firebase from "firebase";
+import fb from "@/firebase/init";
+export default {
+  name: "Signup",
+  data() {
+    return {
+      name: "",
+      errorText: null,
+      email: "",
+      password: "",
+      userList: [],
     };
+  },
+  created() {
+    let ref = fb.collection("username");
+
+    ref.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.userList.push(doc.data().name);
+          //debugger;
+        }
+      });
+    });
+  },
+  methods: {
+    signup: function() {
+      if (!this.name) {
+        this.errorText = "Please enter a name first!";
+        return;
+      }
+
+      function assignRandom(name) {
+        let randomNumber1 = Math.floor(Math.random() * 10);
+        let randomNumber2 = Math.floor(Math.random() * 10);
+        let randomNumber3 = Math.floor(Math.random() * 10);
+        let randomNumber4 = Math.floor(Math.random() * 10);
+        name =
+          name +
+          " #" +
+          randomNumber1 +
+          randomNumber2 +
+          randomNumber3 +
+          randomNumber4;
+        return name;
+      }
+      this.name = assignRandom(this.name);
+      while (this.userList.includes(this.name)) {
+        this.name = assignRandom(this.name);
+      }
+      this.errorText = null;
+      // Log the user in
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then((result) => {
+          const user = result.user;
+          user.updateProfile({ displayName: this.name });
+          alert("Account has been created successfully!");
+
+          // Add username to database
+          fb.collection("username")
+            .add({
+              name: this.name,
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+
+          // Redirect to sign in Page
+          this.$router.push({ name: "Signin" });
+        })
+        .catch((error) => {
+          alert(error.message);
+          console.log(error.code);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-    .login{
-        max-width: 450px;
-        margin-top: 50px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
+.login {
+  max-width: 450px;
+  margin-top: 50px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
 
-    .SignIn
-    {
-        max-width: 450px;
-        margin-top: 25px;
-        margin-bottom: 25px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        background-color: blue;
-    }
+.SignIn {
+  max-width: 450px;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: blue;
+}
 </style>
