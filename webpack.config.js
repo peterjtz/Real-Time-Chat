2827f29623e1107b3e6c620b52c96939cfe122b0
peterjtz <jiaotianze123@gmail.com>
2022-10-06 14:23:55 -0700

    make it better

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin'); //brotli
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new CompressionPlugin({
+    filename: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: /\.(js|css|html|svg)$/,
+    threshold: 8192,
+    minRatio: 0.8
+    }),
+    new BrotliPlugin({ //brotli plugin
+        asset: '[path].br[query]',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    }),
+    new BundleAnalyzerPlugin()
+  ]
+}
