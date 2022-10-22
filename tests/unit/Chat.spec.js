0558fab4fc2f4aefd1e11bd27cb32a4c8a61cfff
peterjtz <jiaotianze123@gmail.com>
2022-10-22 11:22:32 -0700

    push wrong test

---
+++ tests/unit/Chat.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount, mount } from "@vue/test-utils";
+import Chat from "@/views/Chat.vue";
+
+const ChatWrapper = shallowMount(Chat);
+
+describe("Chat", () => {
+    it("does h2 title exists",() => {
+        expect(ChatWrapper.find("h2").text()).toBe("Real-Time Cha");
+    });
+
+    it("does h5 title exists",() => {
+        expect(ChatWrapper.find("h5").text()).toBe("Powered by Vue.js & Firebase");
+    });
+    
+    it("renders the correct message", () => {
+        expect(ChatWrapper.text().split('\n')[0]).toBe("Sign Out");
+        expect(ChatWrapper.text().split('\n')[1]).toBe("       Real-Time Chat Powered by Vue.js & Firebase ");
+    });
+});
